aeffd7f9a1e9a93eb77dbbf75fecac4e677f5b7c
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-06-27 16:57:10 +0900

    '헤더추가'

--- vite-project/src/components/PageHeader.vue
+++ vite-project/src/components/PageHeader.vue
@@ -8,7 +8,7 @@ export default{
   <header class="inset-x-0">
     <nav class="flex items-center justify-between py-4 px-6 lg:px-8" aria-label="Global">
       <router-link to="/">
-        <img src="../assets/vue.svg" alt="logo" style="width: 70px; height: 70px;" class="me-6">
+        <img src="../assets/영진전문대 마크.png" alt="logo" style="width: 70px; height: 70px;" class="me-6">
       </router-link>
       <div class="hidden lg:flex lg:gap-x-16">
         <router-link to="/intro" class="text-m font-semibold leading-6 text-gray-900">소개</router-link>
