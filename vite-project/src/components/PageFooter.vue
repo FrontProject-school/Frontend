d0c151ebaad9f0f5bb5256e78532bb8e00ec02a6
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-07-03 16:17:59 +0900

    readPage ui완성

--- vite-project/src/components/PageFooter.vue
+++ vite-project/src/components/PageFooter.vue
@@ -7,7 +7,7 @@ export default{
 <template>
   
  <footer class="text-center">
-    <div class="border-t border-gray mt-10">
+    <div class="border-t border-gray mt-10 mx-7">
       <div class="text-center space-x-0 text-zinc-400 text-leading-tight ">
        <p class="p-4">영진전문대학교 국제교류원</p>
        <p class="p-4">41527 대구광역시 북구 복현로 35(복현2동 218)영진전문대학교 본관 115호, 대표전화:053-940-5630, 이메일:intl@yju.ac.kr</p>
--- vite-project/src/view/ReadPage.vue
+++ vite-project/src/view/ReadPage.vue
@@ -15,11 +15,12 @@ export default {
   <div class="container mx-auto">
     <div class="my-3">
       <h1 class="text-2xl underline underline-offset-8">제목</h1>
-      <div class="flex justify-between">
-        <div class="mt-5 font-semibold">작성자 : 글쓴이</div>
+      <div class="flex justify-between mt-5 ">
+        <div class="font-semibold">작성자 : 글쓴이</div>
+        <!-- vue - dropdown -->
         <Menu as="div" class="relative inline-block text-left">
           <div>
-            <MenuButton class="hover:bg-gray-50">
+            <MenuButton class="hover:bg-gray-50 ">
               <svg class="origin-center rotate-90 " xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30" aria-hidden="true">
                 <path d="M207.858-432Q188-432 174-446.142q-14-14.141-14-34Q160-500 174.142-514q14.141-14 34-14Q228-528 242-513.858q14 14.141 14 34Q256-460 241.858-446q-14.141 14-34 14Zm272 0Q460-432 446-446.142q-14-14.141-14-34Q432-500 446.142-514q14.141-14 34-14Q500-528 514-513.858q14 14.141 14 34Q528-460 513.858-446q-14.141 14-34 14Zm272 0Q732-432 718-446.142q-14-14.141-14-34Q704-500 718.142-514q14.141-14 34-14Q772-528 786-513.858q14 14.141 14 34Q800-460 785.858-446q-14.141 14-34 14Z"/>
               </svg>
@@ -44,19 +45,18 @@ export default {
         </Menu>
       </div>
     </div>
+    
+    <!-- CONTENT -->
     <div class="" style="min-height: 400px;">
       <div >content</div>
     </div>
-    <!-- <router-link tag="button" to="/" class="write_btn">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 border border-blue-700 rounded">수정</button>
-    </router-link>
-    <router-link tag="button" to="/" class="write_btn">
-      <button class="bg-blue-500 ml-3 hover:bg-blue-700 text-white font-bold py-1 px-4 border border-blue-700 rounded">삭제</button>
-    </router-link> -->
 
-
-    <div class="comment">
-      <h1> 댓글 목록</h1>
+    
+    <!-- COMMENT -->
+    <div class="my-3">
+      <h1 class="ml-1"> 댓글 목록</h1>
+      <div class="border-t border-black my-2"></div>
+      <input type="text" placeholder="작성할 내용을 입력해주세요.">
     </div>
   </div>
 
