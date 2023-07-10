3e7a6a80c669d038e83b476aa282796f169bff53
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-07-10 14:58:32 +0900

    'applypage수정'

--- vite-project/src/view/ApplyPage.vue
+++ vite-project/src/view/ApplyPage.vue
@@ -1,6 +1,6 @@
-<script>
+<!-- <script>
    
-    const HOST = "https://jsonplaceholder.typicode.com/posts/1/comments";
+const HOST = "https://jsonplaceholder.typicode.com/posts/1/comments";
 
 
  
@@ -56,9 +56,10 @@ export default {
     }
     
    
+   
 
 
-};
+ };
 
 </script>
 
@@ -76,7 +77,7 @@ export default {
   <div v-for="item in applyList" :key="item.id">
     <router-link to="/register" class="group block max-w-sm mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 my-8">
         <div class="flex items-center space-x-3">
-            <svg class="h-6 w-6 stroke-sky-500 group-hover:stroke-white" fill="none" viewBox="0 0 24 24"><!-- ... --></svg>
+            <svg class="h-6 w-6 stroke-sky-500 group-hover:stroke-white" fill="none" viewBox="0 0 24 24"></svg>
                 <h3 class="text-slate-900 group-hover:text-white font-semibold text-xl">{{ item.title }}</h3>
         </div>
        <p class="text-slate-500 group-hover:text-white text-sm"> 진행기간 : {{ item.startDate }} ~ {{ item.endDate }}</p>
@@ -84,7 +85,9 @@ export default {
     </router-link>
    </div> 
     
-  <!--<router-link to="/register" class="group block max-w-sm mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 my-8">
+  
+  
+   <router-link to="/register" class="group block max-w-sm mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 my-8">
         <div class="flex items-center space-x-3">
             <svg class="h-6 w-6 stroke-sky-500 group-hover:stroke-white" fill="none" viewBox="0 0 24 24"></svg>
                 <h3 class="text-slate-900 group-hover:text-white font-semibold text-xl">제 1회 버디프로그램</h3>
@@ -100,7 +103,7 @@ export default {
         </div>
        <p class="text-slate-500 group-hover:text-white text-sm">일시 : 2023-09-21 오후 3시</p>
        <p class="text-slate-500 group-hover:text-white text-xs">* 누르면 신청 페이지로 이동</p>
-    </router-link>-->
+    </router-link>
 
 
     
@@ -112,4 +115,76 @@ export default {
 
 <style>
 
-</style>
+</style> -->
+
+<template>
+    <div class="list flex justify-center">
+      <p class="tit text-black text-2xl font-semibold my-4">
+        신청 진행 현황
+      </p>
+    </div>
+  
+    <div class="border-double border-4 border-black mx-60"></div>
+  
+    <div v-for="item in applyList" :key="item.id">
+      <router-link
+        to="/register"
+        class="group block max-w-sm mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500 my-8"
+      >
+        <div class="flex items-center space-x-3">
+          <svg
+            class="h-6 w-6 stroke-sky-500 group-hover:stroke-white"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            
+          </svg>
+          <h3 class="text-slate-900 group-hover:text-white font-semibold text-xl">
+            {{ item.title }}
+          </h3>
+        </div>
+        <p class="text-slate-500 group-hover:text-white text-sm">
+          진행기간: {{ item.startDate }} ~ {{ item.endDate }}
+        </p>
+        <p class="text-slate-500 group-hover:text-white text-xs">
+          * 누르면 신청 페이지로 이동
+        </p>
+      </router-link>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  
+  export default {
+    data() {
+      return {
+        applyList: [],
+      };
+    },
+    mounted() {
+    this.generateData();
+    },
+    methods: {
+    
+    generateData() {
+        
+        const url = 'https://jsonplaceholder.typicode.com/posts/1/comments';
+        axios
+          .get(url)
+          .then(response => {
+            this.applyList = response.data.map(item => ({
+              id: item.id,
+              title: item.name,
+              startDate: item.startDate,
+              endDate: item.endDate,
+            }));
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      },
+    },
+  };
+  </script>
