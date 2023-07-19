309cf5a3d71c229dba86a8c8a19608dc550f4305
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-07-19 16:48:26 +0900

    오류 해결

--- vite-project/src/view/NoticeBoard.vue
+++ vite-project/src/view/NoticeBoard.vue
@@ -36,18 +36,21 @@
 <template>
   <div class="container m-auto">
     <div class="grid gird-cols-6 gap-4 my-5">
-      <div class="col-start-2 col-span-4">
+
+      <!-- 테이블 박스 -->
+      <div class="col-start-1 col-span-3">
+        <!-- 게시판 -->
         <table class="container table-auto text-gray-900 text-center m-auto">
           <thead>
-            <tr class="border-y-2">
+            <tr class="border-y-2 bg-slate-100">
               <th class="p-3">제목</th>
               <th>작성자</th>
               <th>작성일</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in noticeList" key="item.id" class="border-b ">
-              <!-- <td class="py-2">{{ item.postId }}</td> -->
+            <!-- 게시글 -->
+            <tr v-for="item in noticeList" key="item.id" class="border-b bg-slate-10">
               <td colspan="2" class="float-left py-3 px-4"><router-link to="/read" >
                 <span class="pr-10 ml-5">{{ item.postId }}</span>
                 <span>{{ item.name }}</span>
@@ -55,8 +58,8 @@
               <td>{{ item.id }}</td>
               <td>2023-05-19</td>
             </tr>
+            <!-- 더미 -->
             <tr v-for="item in noticeList" key="item.id" class="border-b ">
-              <!-- <td class="py-2">{{ item.postId }}</td> -->
               <td colspan="2" class="float-left py-3 px-4"><router-link to="/read" >
                 <span class="pr-10 ml-5">{{ item.postId }}</span>
                 <span>{{ item.name }}</span>
@@ -70,7 +73,23 @@
           <button class=" rounded-md p-2 text-gray-700 float-right border">글쓰기</button>
         </router-link>
       </div>
-      <div class="col-end-7 col-span-1"></div>
+
+      <!-- 검색 창 -->
+      <div class="col-end-5 col-span-1">
+        <div class="border-2 h-2/3 ml-5">
+          <div class="">
+            <div class="text-2xl text-center m-5">SEARCH</div>
+            <div class="flex justify-center">
+              <section>
+                <div class="flex items-center border-slate-550 border-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -960 960 960"><path d="M796-121 533-384q-30 26-69.959 40.5T378-329q-108.162 0-183.081-75Q120-479 120-585t75-181q75-75 181.5-75t181 75Q632-691 632-584.85 632-542 618-502q-14 40-42 75l264 262-44 44ZM377-389q81.25 0 138.125-57.5T572-585q0-81-56.875-138.5T377-781q-82.083 0-139.542 57.5Q180-666 180-585t57.458 138.5Q294.917-389 377-389Z"/></svg>
+                  <input type="text" class=" p-1 ">
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
