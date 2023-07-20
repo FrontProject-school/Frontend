fc74994c12922f10a816c439538de7ccf25cb254
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-07-20 14:58:44 +0900

    게시판 라우트 병합

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Home from '../view/Home.vue'
 import IntroPage from '../view/IntroPage.vue'
-import NoticeBoard from '../view/NoticeBoard.vue'
+import BoardPage from '../view/BoardPage.vue'
 import ReadPage from '../view/ReadPage.vue'
 import ApplyPage from '../view/ApplyPage.vue'
 import RegisterPage from '../view/RegisterPage.vue'
@@ -11,50 +11,57 @@ import LoginModalPage from '../view/LoginModalPage.vue'
 import SignUpPage from '../view/SignUpPage.vue'
 
 
+const routes = [
+  {
+      path: '/',
+      component: Home
+  },
+  {
+      path: '/intro',
+      component: IntroPage
+  },
+  {
+      path: '/board/notice',  // 공지 게시판
+      component: BoardPage
+  },
+  {
+      path: '/board/Inquiry', // 문의 게시판
+      component: BoardPage
+  },
+  {
+      path: '/board/bulletin',    // 자유 게시판
+      component: BoardPage
+  },
+  {
+      path: '/read',
+      component: ReadPage
+  },
+  {
+      path:'/apply',
+      component:ApplyPage
+  },
+  {
+      path:'/register',
+      component:RegisterPage
+  },
+  {
+      path: '/write',
+      component:WritePage
+  },
+  {
+      path:'/modal' ,
+      component:LoginModalPage
+  },
+  {
+      path :'/signup',
+      component: SignUpPage
+  }
+]
 
-export default createRouter({
-    history: createWebHashHistory(),
-
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/intro',
-            component: IntroPage
-        },
-        {
-            path: '/notice',
-            component: NoticeBoard
-        },
-        {
-            path: '/read',
-            component: ReadPage
-        },
-        {
-            path:'/apply',
-            component:ApplyPage
-        },
-        {
-            path:'/register',
-            component:RegisterPage
-        },
-        {
-            path: '/write',
-            component:WritePage
-        },
-        {
-            path:'/modal' ,
-            component:LoginModalPage
-        },
-        {
-            path :'/signup',
-            component: SignUpPage
-
-        }
-       
-
-
-    ]
+const router = createRouter({
+  mode: 'history',
+  history: createWebHistory(),
+  routes
 })
+
+export default router
--- vite-project/src/view/BoardPage.vue
+++ vite-project/src/view/BoardPage.vue
@@ -1,7 +1,9 @@
 <script>
+  let url = window.location.pathname
+  // 게시판 경로에 따른 api 경로
+  // const HOST = `http://localhost:5174${url}`;
   const HOST = "https://jsonplaceholder.typicode.com/posts/1/comments";
 
-  console.log("notice");
   export default {
     mounted() {
     console.log("notice");
@@ -20,6 +22,7 @@
             console.log(res);
             this.noticeList = res.data;
             console.log(this.noticeList);
+            console.log(url)
           })
           .catch((error) => {
           console.log("Error occurred:");
@@ -38,7 +41,7 @@
     <div class="grid gird-cols-6 gap-4 my-5">
 
       <!-- 테이블 박스 -->
-      <div class="col-start-1 col-span-3">
+      <div class="col-start-1 col-span-4">
         <!-- 게시판 -->
         <table class="container table-auto text-gray-900 text-center m-auto">
           <thead>
@@ -75,15 +78,15 @@
       </div>
 
       <!-- 검색 창 -->
-      <div class="col-end-5 col-span-1">
+      <div class="col-end-6 col-span-1">
         <div class="border-2 h-2/3 ml-5">
           <div class="">
-            <div class="text-2xl text-center m-5">SEARCH</div>
+            <div class="text-2xl text-center m-5">검색창</div>
             <div class="flex justify-center">
               <section>
                 <div class="flex items-center border-slate-550 border-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -960 960 960"><path d="M796-121 533-384q-30 26-69.959 40.5T378-329q-108.162 0-183.081-75Q120-479 120-585t75-181q75-75 181.5-75t181 75Q632-691 632-584.85 632-542 618-502q-14 40-42 75l264 262-44 44ZM377-389q81.25 0 138.125-57.5T572-585q0-81-56.875-138.5T377-781q-82.083 0-139.542 57.5Q180-666 180-585t57.458 138.5Q294.917-389 377-389Z"/></svg>
-                  <input type="text" class=" p-1 ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="mx-1" width="20" height="20" viewBox="0 -960 960 960"><path d="M796-121 533-384q-30 26-69.959 40.5T378-329q-108.162 0-183.081-75Q120-479 120-585t75-181q75-75 181.5-75t181 75Q632-691 632-584.85 632-542 618-502q-14 40-42 75l264 262-44 44ZM377-389q81.25 0 138.125-57.5T572-585q0-81-56.875-138.5T377-781q-82.083 0-139.542 57.5Q180-666 180-585t57.458 138.5Q294.917-389 377-389Z"/></svg>
+                  <input type="text" class=" p-1 outline-0" placeholder="검색어를 입력해주세요.">
                 </div>
               </section>
             </div>
