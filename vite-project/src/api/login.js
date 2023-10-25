6927dec86fa8ee1e07c3cdf79b9ab371747b674a
leehyobin2 <113012132+hyobinaaa@users.noreply.github.com>
2023-10-25 20:09:45 +0900

    login

---
+++ vite-project/src/api/login.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// 로그인
+async function loginPost(postData) {
+    try {
+      const response = await axios.post("/login", postData); 
+      return response.data;
+    } catch (error) {
+      console.error("에러 발생:", error);
+      throw error;
+    }
+  }
+// 로그인
+  async function loginOutPost(postData) {
+    try {
+      const response = await axios.post("/logout", postData); 
+      return response.data;
+    } catch (error) {
+      console.error("에러 발생:", error);
+      throw error;
+    }
+  }
+
+  
+export {
+    loginPost,
+    loginOutPost
+  
+  };
+  
